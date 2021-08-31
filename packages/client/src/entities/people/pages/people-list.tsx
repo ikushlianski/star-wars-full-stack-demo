f6224417ffd62d44dc0d723d702e5c8a7c7d2032
Ilya Kushlianski <kushliansky@gmail.com>
2021-08-31 23:27:51 +0300

    Retain query params when returning from person details, stay on same page

--- packages/client/src/entities/people/pages/people-list.tsx
+++ packages/client/src/entities/people/pages/people-list.tsx
@@ -1,51 +1,112 @@
 import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import { Paper, TableContainer } from '@material-ui/core';
+import { useHistory } from 'react-router-dom';
+
 import { getPeopleChunk, getPeopleCount } from '../store/selectors';
 import { PaginationComponent } from '../components/pagination';
 import { fetchPeople, fetchPeopleTotalCount } from '../store/action';
+import { TableComponent } from '../components/table';
 import { SortBy } from '../../../contracts/sort-by';
 import { SortDir } from '../../../contracts/sort-dir';
-import { TableComponent } from '../components/table';
-import { Paper, TableContainer } from '@material-ui/core';
+import { getQueryVariable } from '../../../helpers/querystring';
 
-export const PeopleList = () => {
+export const PeopleList: React.FC = () => {
   const limit = 10;
   const [page, setPage] = useState(0);
-  const [sortBy, setSortBy] = useState(SortBy.Age);
-  const [sortDir, setSortDir] = useState(SortDir.DESC);
-  const offset = page * limit;
+  const [sortDir, setSortDir] = useState<SortDir>(SortDir.DESC);
+  const [sortBy, setSortBy] = useState<SortBy>(SortBy.Age);
 
+  const history = useHistory();
   const dispatch = useDispatch();
   const peopleCount = useSelector(getPeopleCount);
   const peopleForPage = useSelector(getPeopleChunk);
 
   const onPageChange = (newPage: number) => {
+    const param = new URLSearchParams();
+
+    param.set('page', newPage.toString());
+    param.set('sortBy', sortBy);
+    param.set('sortDir', sortDir);
+    param.set('limit', limit.toString());
+
+    localStorage.setItem('SearchQuery', param.toString());
+
+    history.push({ search: param.toString() });
+
     setPage(newPage);
   };
 
   const handleSetSortBy = (newSortBy: SortBy) => {
+    const param = new URLSearchParams();
+
+    param.set('page', '0');
+    param.set('sortBy', newSortBy);
+    param.set('sortDir', sortDir);
+    param.set('limit', limit.toString());
+
     setSortBy(newSortBy);
     setSortDir(SortDir.ASC);
     setPage(0);
+
+    localStorage.setItem('SearchQuery', param.toString());
+
+    history.push({ search: param.toString() });
   };
 
   const handleSetSortDir = (newSortDir: SortDir) => {
+    const param = new URLSearchParams();
+
+    param.set('page', '0');
+    param.set('sortBy', sortBy);
+    param.set('sortDir', newSortDir);
+    param.set('limit', limit.toString());
+
     setSortDir(newSortDir);
     setPage(0);
+
+    localStorage.setItem('SearchQuery', param.toString());
+
+    history.push({ search: param.toString() });
   };
+
+  useEffect(() => {
+    const qs = localStorage.getItem('SearchQuery');
+
+    const param = new URLSearchParams();
+
+    if (!qs) {
+      param.set('page', '0');
+      param.set('sortBy', SortBy.Age);
+      param.set('sortDir', SortDir.DESC);
+      param.set('limit', limit.toString());
+
+      const newQs = param.toString();
+
+      history.push({ search: newQs });
+
+      localStorage.setItem('SearchQuery', newQs);
+    } else {
+      setPage(Number(getQueryVariable('page')));
+      setSortDir(getQueryVariable('sortDir') as SortDir);
+      setSortBy(getQueryVariable('sortBy') as SortBy);
+
+      history.push({ search: qs });
+    }
+  }, [history]);
 
   useEffect(() => {
     dispatch(
       fetchPeople({
         sortBy,
         sortDir,
-        offset,
+        offset: page * limit,
         limit,
       }),
     );
 
     dispatch(fetchPeopleTotalCount());
-  }, [dispatch, offset, page, sortBy, sortDir]);
+  }, [dispatch, page, sortBy, sortDir]);
 
   return (
     <div>
